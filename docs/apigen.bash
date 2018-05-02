a981dbd842732ea7e4f1da1384ab33f822f6f5a7
Chilipp <p.sommer@posteo.de>
2018-05-02 16:23:07 +0200

    Setup for gnu-sed

--- docs/apigen.bash
+++ docs/apigen.bash
@@ -3,5 +3,5 @@
 # sphinx-apidoc and sed
 sphinx-apidoc -f -M -e  -T -o api ../straditize/ ../straditize/ocr.py
 # replace chapter title in sphinx_nbexamples.rst
-sed -i '' -e 1,1s/.*/'API Reference'/ api/straditize.rst
-sed -i '' -e 2,2s/.*/'============='/ api/straditize.rst
+sed -i -e 1,1s/.*/'API Reference'/ api/straditize.rst
+sed -i -e 2,2s/.*/'============='/ api/straditize.rst
