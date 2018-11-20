3a6daf7ea80ad7dc17fe41a5f53b7a232fc18230
Philipp Sommer <philipp.sommer@unil.ch>
2018-11-20 14:56:04 +0100

    Minor bug fixes

--- straditize/widgets/data.py
+++ straditize/widgets/data.py
@@ -1743,8 +1743,8 @@ class BarSplitter(QTreeWidget, StraditizerControlBase):
                 if y == hline.get_segments()[0][0, 1]:
                     hline.remove()
                     self.lines.remove(hline)
-                    self.straditizer.data_reader.draw_figure()
-                    break
+                self.straditizer.draw_figure()
+                self.suggestions_fig.canvas.draw()
 
     def _get_col(self, item):
         """Convenience method for getting the column of a toplevel item"""
