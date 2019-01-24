0965724fe046b281ee29c982c6c64bf6fd17cdf9
Chilipp <p.sommer@posteo.de>
2019-01-24 14:37:49 +0100

    increased version and added logo

--- straditize/version.py
+++ straditize/version.py
@@ -17,4 +17,4 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>."""
-__version__ = '0.0.1.dev0'
+__version__ = '0.1.0'
