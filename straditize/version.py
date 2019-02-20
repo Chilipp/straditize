75bc294b9a3e9c292049157b88883287d2c82394
Chilipp <p.sommer@posteo.de>
2019-02-20 04:04:47 +0100

    increased version

--- straditize/version.py
+++ straditize/version.py
@@ -17,4 +17,5 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>."""
-__version__ = '0.1.1'
+__version__ = '0.1.2.dev0'
+
