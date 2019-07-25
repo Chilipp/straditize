723b80568aa7a899c971c930febef8f714da41ac
Philipp Sommer <philipp.sommer@unil.ch>
2019-07-25 08:53:38 +0200

    Added more scenarios

--- straditize/evaluator.py
+++ straditize/evaluator.py
@@ -1,5 +1,7 @@
 """Evaluator class for the straditize algorithms"""
 import os.path as osp
+import shutil
+import multiprocessing as mp
 import warnings
 from PIL import Image
 from functools import partial
@@ -9,6 +11,7 @@ from collections import OrderedDict
 from straditize.common import docstrings
 from straditize.straditizer import Straditizer
 from psy_strat.stratplot import stratplot
+from itertools import filterfalse
 
 
 docstrings.get_sectionsf('stratplot')(stratplot)
@@ -134,7 +137,7 @@ class StraditizeEvaluator:
         xwidths = pd.Series(
             np.diff([ax.get_xlim() for ax in self.sp.axes]).ravel(), columns)
 
-        df = self.data.fillna(0) * widths / xwidths
+        df = (self.data.fillna(0) * widths / xwidths).round()
 
         y_px = self.data_ylim - self.data_ylim[0]
         y_data = next(np.sort(ax.get_ylim()) for ax in self.sp.axes)
@@ -247,6 +250,12 @@ class StraditizeEvaluator:
         return pd.DataFrame(interpolated, index=pd.Index(np.arange(height)),
                             columns=np.arange(ncols)).fillna(0)
 
+    def set_xtranslation(self, stradi):
+        stradi.data_reader.xaxis_px = np.array([0, 1])
+        dx = np.diff(self.sp[0].psy.ax.get_xlim())
+        stradi.data_reader.xaxis_data = np.r_[
+            0, dx / np.diff(self.column_bounds[0])]
+
     def init_stradi(self, datalim=True, columns=True, names=True,
                     digitize=True, samples=True, axes=False):
         path = self.filepath
@@ -272,8 +281,7 @@ class StraditizeEvaluator:
         if names:
             stradi.colnames_reader.column_names = self.data.columns.tolist()
 
-        stradi.data_reader.xaxis_px = self.column_bounds[0]
-        stradi.data_reader.xaxis_data = np.array(self.sp[0].psy.ax.get_xlim())
+        self.set_xtranslation(stradi)
 
         if digitize:
             stradi.data_reader.full_df = self.full_df
@@ -285,26 +293,26 @@ class StraditizeEvaluator:
 
         return stradi
 
-    def evaluate_column_starts(self, close=True):
+    def evaluate_column_starts(self, close=True, base='starts_'):
         stradi = self.init_stradi(columns=False, axes=True)
         starts = stradi.data_reader._get_column_starts()
         ref = self.column_starts
         results = self.results.copy()
         missing_cols = len(ref) - len(starts)
-        results['starts_missmatch'] = 100 * (len(starts) - len(ref)) / len(ref)
-        results['starts_missing'] = missing_cols
+        results[base + 'missmatch'] = 100 * (len(starts) - len(ref)) / len(ref)
+        results[base + 'missing'] = missing_cols
 
         if not missing_cols:
             diff = np.abs(starts - ref).sum()
             width = np.diff(self.data_xlim)[0]
-            results['starts_diff'] = diff
-            results['starts_diff_perc'] = diff / width * self.summed_perc
+            results[base + 'diff'] = diff
+            results[base + 'diff_perc'] = diff / width * self.summed_perc
 
             # now remove the yaxes
             stradi.data_reader.recognize_yaxes(remove=True)
             diff = np.abs(stradi.data_reader.column_starts - ref).sum()
-            results['starts_diff_removey'] = diff
-            results['starts_diff_removey_perc'] = \
+            results[base + 'diff_removey'] = diff
+            results[base + 'diff_removey_perc'] = \
                 diff / width * self.summed_perc
 
         self.results = results
@@ -314,36 +322,55 @@ class StraditizeEvaluator:
         stradi = self.init_stradi(columns=False, axes=True)
         stradi_ref = self.init_stradi(digitize=False)
 
+        # get the vertical axes that are the difference between the two images
+        orig = stradi.data_reader.binary.copy()
+        ref = (orig - stradi_ref.data_reader.binary).astype(bool)
+
         # remove the y-axes
         stradi.data_reader._get_column_starts()
         stradi.data_reader.recognize_yaxes(remove=True)
+        sim = (orig - stradi.data_reader.binary).astype(bool)
         results = self.results
-        # transform into black-and-white image
-        im1 = np.where(stradi.data_reader.binary.astype(bool), 0, 255)
-        im2 = np.where(stradi_ref.data_reader.binary.astype(bool), 0, 255)
+
+        ref_sum = ref.sum()
+        # pixels that have wrongly considered as being part of the y-axes
+        false_positive = sim & (~ref)
+        # pixels that have wrongly not considered as being part of the y-axis
+        false_negative = (~sim) & ref
+        # pixels that have been identified correctly
+        correct = sim & ref
         # calculate rmse
-        results['yaxes_rmse'] = rmse(im1, im2)
+        results['yaxes_false_pos'] = 100 * false_positive.sum() / ref_sum
+        results['yaxes_false_neg'] = 100 * false_negative.sum() / ref_sum
+        results['yaxes_correct'] = 100 * correct.sum() / ref_sum
+
         self.results = results
 
         return (stradi.close(), stradi_ref.close()) if close else (
             stradi, stradi_ref)
 
-    def evaluate_sample_accuracy(self, close=True):
-        stradi = self.init_stradi(digitize=False)
+    def evaluate_sample_accuracy(self, close=True, stradi=None,
+                                 base='samples_'):
+        stradi = stradi or self.init_stradi(digitize=False)
         stradi.data_reader.digitize()
+        if stradi.data_reader.exaggerated_reader is not None:
+            stradi.data_reader.digitize_exaggerated()
         results = self.results
         ref = self.data.fillna(0)
         full_df = stradi.full_df
         indexes = list(map(partial(full_df.index.get_loc, method='nearest'),
                            ref.index))
         sim = full_df.iloc[indexes]
-        results['samples_rmse'] = rmse(sim.values,
-                                       ref.values)
+        s = ref.size
+        results[base + 'rmse'] = rmse(sim.values, ref.values)
+        results[base + 'too_high'] = 100 * (sim.values > ref.values).sum() / s
+        results[base + 'too_low'] = 100 * (sim.values < ref.values).sum() / s
         self.results = results
         return stradi.close() if close else stradi
 
-    def evaluate_sample_position(self, close=True):
-        stradi = self.init_stradi(samples=False)
+    def evaluate_sample_position(self, close=True, stradi=None,
+                                 base='samples_'):
+        stradi = stradi or self.init_stradi(samples=False)
         stradi.data_reader.add_samples(
             *stradi.data_reader.find_samples())
 
@@ -353,8 +380,8 @@ class StraditizeEvaluator:
         nref = len(ref)
 
         results = self.results
-        results['samples_missmatch'] = 100 % abs(nfound - nref)/ nref
-        results['samples_missing'] = nref - nfound
+        results[base + 'missmatch'] = 100 % abs(nfound - nref)/ nref
+        results[base + 'missing'] = nref - nfound
 
         closest = list(map(
             partial(final.index.get_loc, method='nearest'),
@@ -363,11 +390,18 @@ class StraditizeEvaluator:
         age_range = ref.index.max() - ref.index.min()
 
         # normalized rmse of the age
-        results['samples_nrmse_y'] = rmse(
+        results[base + 'nrmse_y'] = rmse(
             final.index[closest].values, ref.index.values) / age_range
 
         self.results = results
         return stradi.close() if close else stradi
+
+    def evaluate_full(self, close=True):
+        stradi = self.evaluate_column_starts(False, 'full_starts_')
+        self.set_xtranslation(stradi)
+        if len(stradi.data_reader.column_starts) == len(self.column_starts):
+            stradi = self.evaluate_sample_accuracy(False, stradi, 'full_')
+            return self.evaluate_sample_position(close, stradi, 'full_')
 
     def run(self):
         """Run all evaluations"""
@@ -375,9 +409,59 @@ class StraditizeEvaluator:
         self.evaluate_yaxes_removal()
         self.evaluate_sample_accuracy()
         self.evaluate_sample_position()
+        self.evaluate_full()
 
-    def close_figs(self):
-        self.sp.close(figs=True)
+    def close(self):
+        self.sp.close(figs=True, data=True, ds=True)
+        del self.sp
+
+
+class NoVerticalsEvaluator(StraditizeEvaluator):
+    """An evaluator for an image without y-axis"""
+
+    def export(self, *args, **kwargs):
+        super().export(*args, **kwargs)
+        shutil.copyfile(self.filepath + '-no-axes.png', self.filepath + '.png')
+
+    def evaluate_column_starts(self, close=True, base='starts_'):
+        stradi = self.init_stradi(columns=False, axes=True)
+        starts = stradi.data_reader._get_column_starts()
+        ref = self.column_starts
+        results = self.results.copy()
+        missing_cols = len(ref) - len(starts)
+        results[base + 'missmatch'] = 100 * (len(starts) - len(ref)) / len(ref)
+        results[base + 'missing'] = missing_cols
+
+        if not missing_cols:
+            diff = np.abs(starts - ref).sum()
+            width = np.diff(self.data_xlim)[0]
+            results[base + 'diff'] = diff
+            results[base + 'diff_perc'] = diff / width * self.summed_perc
+
+        self.results = results
+        return stradi.close() if close else stradi
+
+    def evaluate_yaxes_removal(self, close=True):
+        return
+
+
+class ExaggerationsEvaluator(StraditizeEvaluator):
+    """An evaluator with exaggerations"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # draw red exaggerations
+        self.sp.update(exag_factor=5, exag='areax',
+                       exag_color=[1, 0, 0, 1])
+
+    def init_stradi(self, *args, **kwargs):
+        stradi = super().init_stradi(*args, **kwargs)
+        if stradi.data_reader is not None and stradi.data_reader.columns:
+            exag = stradi.data_reader.create_exaggerations_reader(5)
+            im = np.asarray(stradi.data_reader.image)
+            mask = (im[..., 0] > 200) & (im[..., 1:-1].sum(axis=-1) < 100)
+            exag.mark_as_exaggerations(mask)
+        return stradi
 
 
 class BaselineScenario:
@@ -386,9 +470,24 @@ class BaselineScenario:
     This class uses the default settings of the :class:`StraditizeEvaluator`
     and runs the analysis for a given dataset from POLNET."""
 
-    def __init__(self, data, output_dir='.'):
-        self.evaluators = []
+    def __init__(self, output_dir='.'):
         self.output_dir = output_dir
+        self.failed = []
+        self._all_results = []
+        self.results = None
+
+    def __reduce__(self):
+        return (self.__class__,
+                (self.output_dir, ),
+                {'failed': self.failed,
+                 'results': self.results,
+                 '_all_results': []
+                 }
+                )  # do not distribute all results
+
+    def run(self, data, processes=None):
+        self.failed.extend(data.e_.unique())
+        all_results = self._all_results
         grouped = data.groupby('e_')
         progress_args = (grouped.ngroups, 'Progress', 'Complete', 50)
 
@@ -396,13 +495,21 @@ class BaselineScenario:
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', 'Distinct samples merged from',
                                     UserWarning)
-            for i, (key, group) in enumerate(grouped, 1):
-                evaluator = self.init_evaluator(key, group)
-                self.export_evaluator(evaluator)
-                evaluator.run()
-                evaluator.close_figs()
-                self.evaluators.append(evaluator)
+            warnings.filterwarnings('ignore', 'divide by zero encountered',
+                                    RuntimeWarning)
+            pool = mp.Pool(processes)
+            for i, results in enumerate(pool.imap_unordered(self, grouped), 1):
+                if np.ndim(results):
+                    all_results.append(results)
+                    self.failed.remove(int(results.name[0]))
                 print_progressbar(i, *progress_args)
+            pool.close()
+            pool.join()
+            pool.terminate()
+        self.results = pd.concat(all_results, axis=1, sort=False).T
+        self.results.index.names = self.index_names
+
+    index_names = ['e_', 'ntaxa', 'nsamples']
 
     def init_evaluator(self, name, data, *args, **kwargs):
         """Initialize an evaluator for a given data set"""
@@ -416,7 +523,68 @@ class BaselineScenario:
         evaluator.export(osp.join(self.output_dir, evaluator.name),
                          *args, **kwargs)
 
-    @property
-    def results(self):
-        return pd.concat([evaluator.results for evaluator in self.evaluators],
-                         axis=1)
+    def __call__(self, data_tuple):
+        key, group = data_tuple
+        try:
+            evaluator = self.init_evaluator(key, group)
+        except Exception:
+            return key
+        else:
+            try:
+                self.export_evaluator(evaluator)
+                evaluator.run()
+            except Exception:
+                evaluator.close()
+                return key
+        results = evaluator.results
+        evaluator.close()
+        return results
+
+
+class DPI600Scenario(BaselineScenario):
+    """Another evaluation scenario but with a resolution of 600 dpi"""
+
+    def export_evaluator(self, *args, **kwargs):
+        kwargs['dpi'] = 600
+        return super().export_evaluator(*args, **kwargs)
+
+
+class DPI150Scenario(BaselineScenario):
+    """Another evaluation scenario but with a resolution of 150 dpi"""
+
+    def export_evaluator(self, *args, **kwargs):
+        kwargs['dpi'] = 150
+        return super().export_evaluator(*args, **kwargs)
+
+
+class BlackWhiteScenario(BaselineScenario):
+    """An evaluation scenario with a binary (black and white) image"""
+
+    def init_evaluator(self, *args, **kwargs):
+        evaluator = super().init_evaluator(*args, **kwargs)
+        evaluator.sp.update(color='k')
+        return evaluator
+
+
+class NoVerticalsScenario(BaselineScenario):
+    """An evaluation scenario without y-axes in the plot"""
+
+    def init_evaluator(self, name, data, *args, **kwargs):
+        """Initialize an evaluator for a given data set"""
+        import matplotlib.pyplot as plt
+        if 'fig' not in kwargs:
+            kwargs['fig'] = plt.figure(figsize=(12, 6))
+        return NoVerticalsEvaluator.from_polnet(
+            data, *args, name=str(name), **kwargs)
+
+
+class ExaggerationsScenario(BaselineScenario):
+    """An evaluation scenario with an exaggerated plot of low percentages"""
+
+    def init_evaluator(self, name, data, *args, **kwargs):
+        """Initialize an evaluator for a given data set"""
+        import matplotlib.pyplot as plt
+        if 'fig' not in kwargs:
+            kwargs['fig'] = plt.figure(figsize=(12, 6))
+        return ExaggerationsEvaluator.from_polnet(
+            data, *args, name=str(name), **kwargs)
